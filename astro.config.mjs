d46ed3bdac215dfab78bae4868942e6d53a0e873
emad_masoud <bohneen@gmail.com>
2026-08-11 00:10:57 +0300

    feat: configure conditional adapter to use node for development and cloudflare for production

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,13 +7,17 @@ import icon from 'astro-icon';
 import sitemap from '@astrojs/sitemap';
 import { ViteToml } from 'vite-plugin-toml';
 import tailwindcss from '@tailwindcss/vite';
-
+import node from '@astrojs/node';
 import cloudflare from '@astrojs/cloudflare';
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: cloudflare({ platformProxy: { enabled: false } }),
+  adapter: isProduction
+    ? cloudflare({ platformProxy: { enabled: true } })
+    : node({ mode: 'standalone' }),
   site: "https://meamart.com",
   integrations: [
     vue(),
